0ad1d394997501ffcd256db69fbf326c10332348
Kotaro <c0a24099c5@edu.teu.ac.jp>
2026-04-14 16:37:14 +0900

    こうかとん常移動

--- pygame_image.py
+++ pygame_image.py
@@ -16,6 +16,7 @@ def main():
 
     kk_rct = kk_img.get_rect() # 練習１０.１
     kk_rct.center = 300,200 # 練習１０.２
+    
     screen.blit(kk_img, kk_rct) # 
 
     tmr = 0
@@ -23,15 +24,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        kk_rct.move_ip((-1, 0))
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, +1))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            kk_rct.move_ip((-1, 0)) # 練習１０.３,４
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0]) # 練習２,５
